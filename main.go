122d90ce6cc599e67c86237e3f102e7f663fed95
Josh Klar <j@iv597.com>
2015-09-25 08:22:52 -0500

    all that boring upfront cmd flag parsing is now out of the way

--- main.go
+++ main.go
@@ -3,32 +3,43 @@ package main
 import (
 	"fmt"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"io"
-	"net/http"
+	//"io"
+	//"net/http"
 )
 
-type taskCommandLine string
+type taskCommandLine []string
 
-func (s *taskCommandLine) Set(value string) taskCommandLine {
-	return fmt.Sprint(*s, value)
+func (s *taskCommandLine) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+func (s *taskCommandLine) String() string {
+	return ""
 }
 
 func (s *taskCommandLine) IsCumulative() bool {
 	return true
 }
 
+func getTaskCmd(s kingpin.Settings) (target *[]string) {
+	target = new([]string)
+	s.SetValue((*taskCommandLine)(target))
+	return
+}
+
 func main() {
 	runCountHelp := "number of parallel executions - if your application is asynchronous, the default of 1 is safe"
-	runCount := kingpin.Flag("count", runCountHelp).Default("1").Short("c").Int()
-	bindTo := kingpin.Flag("bind", "bind address (IP/hostname)").Default("localhost").Short("b").String()
-	portNum := kingpin.Flag("port", "port to listen on").Default("8005").Short("p").Int()
+	runCount := kingpin.Flag("count", runCountHelp).Default("1").Short('c').Int()
+	bindTo := kingpin.Flag("bind", "bind address (IP/hostname)").Default("localhost").Short('b').String()
+	portNum := kingpin.Flag("port", "port to listen on").Default("8005").Short('p').Int()
 	taskPortsHelp := "comma-separated list (length of `count`) of ports to use for spawned processes"
-	taskPorts := kingpin.Flag("innerPorts", taskPortsHelp).Short("P").String()
+	taskPorts := kingpin.Flag("innerPorts", taskPortsHelp).Short('P').String()
 
-	task := taskCommandLine(kingpin.Arg("command", "task to shuffle"))
+	task := getTaskCmd(kingpin.Arg("command", "task to shuffle"))
 
 	kingpin.Version("0.0.0")
 	kingpin.Parse()
 
-	fmt.Printf("")
+	fmt.Printf("%v%v%v%v  %v", *runCount, *bindTo, *portNum, *taskPorts, *task)
 }
